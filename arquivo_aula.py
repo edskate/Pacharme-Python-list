7d9093691103a2f3b031aa4da0fd26a4c4a96249
edilson gomes de lima <edskateicevida@gmail.com>
2023-09-17 00:43:59 -0300

    list copy

--- arquivo_aula.py
+++ arquivo_aula.py
@@ -13,3 +13,43 @@ texto_b = texto_a
 texto_b = texto_b.replace("L", "B")
 print(texto_a)
 print(texto_b)
+
+
+
+# 2 formas
+
+lista_c = lista_a[:]
+lista_c.append("Arroz")
+print(lista_a)
+print(lista_c)
+
+
+# 3 formas 
+
+
+lista_c = lista_a.copy()
+lista_c.append("Arroz")
+print(lista_a)
+print(lista_c)
+
+# para listas de listas, temos que pensar diferente
+
+
+produtos = [
+    ["Ipad", 5000],
+    ["Iphone", 4500],
+    
+    
+]
+
+produtos2 = produtos[:]
+produtos2[0][1] = 6000
+print(produtos)
+print(produtos2)
+
+# solução é um deepcopy
+import copy
+produtos3 = copy.deepcopy(produtos)
+produtos3[0][1] = 1000
+print(produtos)
+print(produtos3)
